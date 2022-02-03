d197f0acc8ff7df51bf81bd468808cbf6b8dde66
rsmarsh <richard@live.cl>
2022-02-03 22:59:42 +0000

    added http req handlers

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 import './src/env.js';
 import * as path from 'path';
 import express from 'express';
+import { setPixel, clearAll } from './src/unicorn.js';
 import { randomColour, randomPixel } from './src/utils.js';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+let pixelsChanged = 0;
 
 const server = express();
 
 server.use(express.static('public'));
 
 
+server.get('/pixel/set/:x/:y/:r/:g/:b', (req, res) => {
+    const x = req.params.x;
+    const y = req.params.y;
+    const r = req.params.r;
+    const g = req.params.g;
+    const b = req.params.b;
+
+    setPixel(x, y, { 
+        r: r,
+        g: g,
+        b: b
+    });
+
+    res.send('changed '+(++pixelsChanged));
 });
+server.get('/pixel/random', (req, res) => {
+    setPixel(
+        randomPixel(),
+        randomPixel(), 
+        { 
+            r: randomColour(),
+            g: randomColour(),
+            b: randomColour()
+        });
+    res.send('changed '+(++pixelsChanged));
+});
+
+server.get('/pixel/clear', (req,res) => {
+    res.send('cleared');
+    clearAll();
+})
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('server up and running');
 });
-
-printHatInfo();
