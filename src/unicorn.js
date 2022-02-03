c6936d01a926bfcc55f7c841e1dae0052020f798
rsmarsh <richard@live.cl>
2022-02-03 22:56:53 +0000

    added more unicorn interactions

--- src/unicorn.js
+++ src/unicorn.js
@@ -4,3 +4,16 @@ const unicornHat = new UnicornHatHD(process.env.UNICORN_ADDRESS);
 export const printHatInfo = () => {
     console.log(unicornHat);
 };
+
+export const setPixel = (x, y, {r,g,b}) => {
+    unicornHat.setPixel(x, y, r, g, b);
+    unicornHat.show();
+};
+
+export const clearAll = () => {
+    unicornHat.clear();
+    unicornHat.show();
+};
+
+clearAll();
+unicornHat.setBrightness(0.1);
