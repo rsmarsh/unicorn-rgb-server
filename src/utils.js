d44a881ddd7fbaa4469efee404385c5829491238
rsmarsh <richard@live.cl>
2022-02-06 01:25:16 +0000

    extra utils added

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,5 @@
-export const randomPixel = () => Math.floor(Math.random()*16);
-export const randomColour = () => Math.floor(Math.random()*255);
+export const randomPixel = () => Math.floor(Math.random() * 16);
+
+export const randomColour = () => Math.floor(Math.random() * 255);
+
+export const rgbToHex = (r, g, b) => "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
